937d8770de4c2d227e55e48aef7efb631c8c30f0
hyeonju <shj040201@naver.com>
2023-05-16 10:12:54 +0900

    수정 3
    코드리뷰에서 받은 피드백 반영해봤습니다

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -24,10 +24,10 @@ class ViewController: UIViewController {
            let num2 = secondTextField.text{
             if let a = Int(num1),
                let b = Int(num2){
-                Display.text = "\(a) + \(b) = \(a+b)"
+                display.text = "\(a) + \(b) = \(a+b)"
             }else{
-                Display.text = "숫자만 입력해주세요."
-                Display.textColor = .black
+                display.text = "숫자만 입력해주세요."
+                display.textColor = .black
             }
         }
     }
@@ -44,10 +44,10 @@ class ViewController: UIViewController {
         if let num1 = firstTextField.text,
            let num2 = secondTextField.text{
             if let a = Int(num1),let b = Int(num2){
-                Display.text = "\(a) - \(b) = \(a-b)"
+                display.text = "\(a) - \(b) = \(a-b)"
             }else{
-                Display.text = "숫자만 입력해주세요."
-                Display.textColor = .black
+                display.text = "숫자만 입력해주세요."
+                display.textColor = .black
             }
         }
     }
@@ -64,10 +64,10 @@ class ViewController: UIViewController {
         if let num1 = firstTextField.text,
            let num2 = secondTextField.text{
             if let a = Int(num1),let b = Int(num2){
-                Display.text = "\(a) X \(b) = \(a*b)"
+                display.text = "\(a) X \(b) = \(a*b)"
             }else{
-                Display.text = "숫자만 입력해주세요."
-                Display.textColor = .black
+                display.text = "숫자만 입력해주세요."
+                display.textColor = .black
             }
         }
     }
@@ -80,7 +80,7 @@ class ViewController: UIViewController {
         button.addTarget(self, action: #selector(divButtonDidtap), for: .touchUpInside)
         return button
     }()
-    enum ErrorMessage : String, Error{
+    enum ErrorMessage: String, Error{
         case DivZero = "0으로 나눌수 없습니다."
     }
     @objc func divButtonDidtap()throws{
@@ -93,17 +93,16 @@ class ViewController: UIViewController {
             }
             
             if b != 0{
-                Display.text = "\(a) / \(b) = \(a/b)"}else{
+                display.text = "\(a) / \(b) = \(a/b)"}else{
                     throw ErrorMessage.DivZero
                 }
         }
         catch ErrorMessage.DivZero{
-            Display.text = ErrorMessage.DivZero.rawValue
-            Display.textColor = .gray
+            display.text = ErrorMessage.DivZero.rawValue
+            display.textColor = .gray
             
         }
     }
-    
     
     
     //텍스트 필드
@@ -119,11 +118,11 @@ class ViewController: UIViewController {
         return textField
     }()
     @objc func live1(){
-        Display.text = "숫자를 모두 입력해주세요."
-        Display.textColor = .gray
+        display.text = "숫자를 모두 입력해주세요."
+        display.textColor = .gray
     }
     
-    lazy var secondTextField : UITextField = {
+    lazy var secondTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "두번째 숫자를 입력해주세요."
         let padding = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
@@ -135,12 +134,12 @@ class ViewController: UIViewController {
         return textField
     }()
     @objc func live2(){
-        Display.text = "버튼을 눌러주세요!"
-        Display.textColor = .black
+        display.text = "버튼을 눌러주세요!"
+        display.textColor = .black
     }
     
     //라벨
-    lazy var Display : UILabel = {
+    lazy var display: UILabel = {
         let label = UILabel()
         label.text = "값을 입력해주세요."
         label.font = .systemFont(ofSize: 15)
@@ -151,63 +150,61 @@ class ViewController: UIViewController {
     }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.addSubview(self.plusButton)
-        self.view.addSubview(self.minButton)
-        self.view.addSubview(self.mulButton)
-        self.view.addSubview(self.divButton)
-        self.view.addSubview(self.firstTextField)
-        self.view.addSubview(self.secondTextField)
-        self.view.addSubview(self.Display)
-        //        self.view.addSubview(self.Display)
+        view.backgroundColor = .white
+        //self.view.addSubview(self.plusButton)
+        //self.view.addSubview(self.minButton)
+        //self.view.addSubview(self.mulButton)
+        //self.view.addSubview(self.divButton)
+        //self.view.addSubview(self.firstTextField)
+        //self.view.addSubview(self.secondTextField)
+        //self.view.addSubview(self.display)
+       
+        //foreach문을 활용한 방법1
+//        [plusButton, minButton, mulButton, divButton, firstTextField, secondTextField, display].forEach{
+//            view.addSubview($0)
+//        }
+        
+        //extension 을  활용한 방법 2
+        view.addSubviews(
+            plusButton, minButton, mulButton, divButton, firstTextField, secondTextField, display
+        )
+        
         
         
         
         
         //레이아웃
         plusButton.snp.makeConstraints{ (make) in
-            make.leading.equalTo(46)
-            make.trailing.equalTo(-47)
+            make.leading.trailing.equalToSuperview().inset(47)
             make.height.equalTo(39)
-            make.centerX.equalToSuperview()
-            make.top.equalTo(320)
+            make.top.equalToSuperview().offset(320)
         }
         minButton.snp.makeConstraints{ (make) in
-            make.leading.equalTo(46)
-            make.trailing.equalTo(-47)
+            make.leading.trailing.equalToSuperview().inset(47)
             make.height.equalTo(39)
-            make.centerX.equalToSuperview()
             make.top.equalTo(plusButton.snp.bottom).offset(10)
-
         }
         mulButton.snp.makeConstraints{ (make) in
-            make.leading.equalTo(46)
-            make.trailing.equalTo(-47)
+            make.leading.trailing.equalToSuperview().inset(47)
             make.height.equalTo(39)
-            make.centerX.equalToSuperview()
             make.top.equalTo(minButton.snp.bottom).offset(10)
         }
         divButton.snp.makeConstraints{ (make) in
-            make.leading.equalTo(46)
-            make.trailing.equalTo(-47)
+            make.leading.trailing.equalToSuperview().inset(47)
             make.height.equalTo(39)
-            make.centerX.equalToSuperview()
             make.top.equalTo(mulButton.snp.bottom).offset(10)
         }
         firstTextField.snp.makeConstraints{(make) in
-            make.leading.equalTo(46)
-            make.trailing.equalTo(-47)
+            make.leading.trailing.equalToSuperview().inset(47)
             make.height.equalTo(39)
-            make.centerX.equalToSuperview()
             make.top.equalTo(157)
         }
         secondTextField.snp.makeConstraints{(make) in
-            make.leading.equalTo(46)
-            make.trailing.equalTo(-47)
+            make.leading.trailing.equalToSuperview().inset(47)
             make.height.equalTo(39)
-            make.centerX.equalToSuperview()
             make.top.equalTo(firstTextField.snp.bottom).offset(10)
         }
-        Display.snp.makeConstraints{(make) in
+        display.snp.makeConstraints{(make) in
             make.width.equalTo(200)
             make.height.equalTo(22)
             make.centerX.equalToSuperview()
